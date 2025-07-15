27b4cd27a09e97ea292c7e038af1aae1573b0493
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-07-15 12:17:33 +0545

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMS</title>
+</head>
+<body>
+    <h2>Welcome to Student Management System</h2>
+    <a href="login_admin.php">Login as Admin</a> 
+    <a href="login_student.php">Login as Student</a>
+</body>
+</html>
